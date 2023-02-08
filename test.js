8e84f7ac82d3245662ae25b2d3869a4a8170c22c
Olena Kaliuzhna <kaliuzhna.el@gmail.com>
2023-02-08 15:12:23 +0200

    change js

--- test.js
+++ test.js
@@ -6,20 +6,24 @@ num1 = Number(num1);
 let num2 = prompt('Enter the second number');
 num2 = Number(num2);
 
+let sum = num1 + num2;
+let sub = num1 - num2;
+let mult = num1 * num2;
+let div = num1 / num2;
 
 switch (operator) {
-    case '+':
-        alert(num1 + num2);
+    case "+":
+        alert(`${num1} + ${num2} = ${sum}`);
         break;
-    case '-':
-        alert(num1 - num2);
+    case "-":
+        alert(`${num1} - ${num2} = ${sub}`);
         break;
-    case '/':
-        alert(num1 / num2);
+    case "*":
+        alert(`${num1} * ${num2} = ${mult}`);
         break;
-    case '*':
-        alert(num1 * num2);
-
+    case "/":
+        alert(`${num1} / ${num2} = ${div}`);
+        break;
     default:
-        alert("Sorry you don`t want to calculate");
+        alert("Sorry that you don`t want to calculate");
 }
